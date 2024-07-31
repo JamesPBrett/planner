61aa0264a899d1790cdab3cc1e6766b2cfbc000a
James Brett <james@jamesbrett.com>
2024-07-31 21:41:36 +0100

    added models

---
+++ models/events.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import List
+
+class Event(BaseModel):
+    id: int
+    title: str
+    image: str
+    description: str
+    tags: List[str]
+    location: str
+    
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "Foo Fighters",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "Foo Fighters in Inverness",
+                "tags": ["Rock", "Music"],
+                "location": "Eden Court"
+            }
+        }
+    
