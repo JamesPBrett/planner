b9f65736c8fd753864622017b95146ce661d5f72
James Brett <james@jamesbrett.com>
2024-08-02 20:04:31 +0100

    set up sqlite connection

--- models/events.py
+++ models/events.py
@@ -1,18 +1,36 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import Optional, List
 
-class Event(BaseModel):
-    id: int
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
+
+    class Config:
+        arbitrary_types_allowed =True
+        json_schema_extra = {
+            "example": {
+                "title": "Foo Fighters",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "Foo Fighters in Inverness",
+                "tags": ["Rock", "Music"],
+                "location": "Eden Court"
+            }
+        }
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[str]
+    location: Optional[str]
     
     class Config:
         json_schema_extra = {
             "example": {
-                "id": 1,
                 "title": "Foo Fighters",
                 "image": "https://linktomyimage.com/image.png",
                 "description": "Foo Fighters in Inverness",
