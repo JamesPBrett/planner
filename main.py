0611cdd8e56cd7188a82fcecbee68cf07353ffe2
James Brett <james@jamesbrett.com>
2024-08-05 21:06:38 +0100

    add mongo db

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from database.connection import conn
-from routes.users import user_router
+
+from database.connection import Settings
 from routes.events import event_router
-import uvicorn
+from routes.users import user_router
 
 app = FastAPI()
 
+settings = Settings()
+
 # Register routes
+
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
 
 
 @app.on_event("startup")
-def on_startup():
-    conn()
+async def init_db():
+    await settings.initialize_database()
 
 
 @app.get("/")
@@ -22,5 +26,5 @@ async def home():
     return RedirectResponse(url="/event/")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
--- models/events.py
+++ models/events.py
@@ -1,29 +1,32 @@
-from sqlmodel import JSON, SQLModel, Field, Column
 from typing import Optional, List
 
+from beanie import Document
+from pydantic import BaseModel
 
-class Event(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+
+class Event(Document):
     title: str
     image: str
     description: str
-    tags: List[str] = Field(sa_column=Column(JSON))
+    tags: List[str]
     location: str
 
     class Config:
-        arbitrary_types_allowed = True
-        json_schema_extra = {
+        schema_extra = {
             "example": {
-                "title": "Foo Fighters",
+                "title": "FastAPI BookLaunch",
                 "image": "https://linktomyimage.com/image.png",
-                "description": "Foo Fighters in Inverness",
-                "tags": ["Rock", "Music"],
-                "location": "Eden Court",
+                "description": "We will be discussing the contents of the FastAPI book in this event.Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
             }
         }
 
+    class Settings:
+        name = "events"
 
-class EventUpdate(SQLModel):
+
+class EventUpdate(BaseModel):
     title: Optional[str]
     image: Optional[str]
     description: Optional[str]
@@ -31,12 +34,12 @@ class EventUpdate(SQLModel):
     location: Optional[str]
 
     class Config:
-        json_schema_extra = {
+        schema_extra = {
             "example": {
-                "title": "Foo Fighters",
+                "title": "FastAPI BookLaunch",
                 "image": "https://linktomyimage.com/image.png",
-                "description": "Foo Fighters in Inverness",
-                "tags": ["Rock", "Music"],
-                "location": "Eden Court",
+                "description": "We will be discussing the contents of the FastAPI book in this event.Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
             }
         }
